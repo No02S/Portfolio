b338008d02ab8820dac82493d3567c29e546f99d
Dima <dima.zabolotno.02@mail.ru>
2022-08-07 17:54:10 +0300

    Test Slayer

--- src/JS/Script.js
+++ src/JS/Script.js
@@ -2,124 +2,19 @@ const slider = tns({
     container: '.slider',
     items: 1,
     slideBy: 'page',
-    nav: true,
-    controls:false,
+    nav: false,
+    controls: false,
     responsive: {
         769: {
-            nav: false
+           
         }   
     }
 });
 
-document.querySelector('.button_left').addEventListener('click', function () {
+document.querySelector('.Prev').onclick = function () {
     slider.goTo('prev');
-});
-document.querySelector('.button_right').addEventListener('click', function () {
+};
+
+document.querySelector('.Next').onclick = function () {
     slider.goTo('next');
-});
-
-$(document).ready(function(){
-    $(function() {
-      
-        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-          $(this)
-            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-        });  
-    });
-
-    function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
-                ;
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-        });
-    };
-
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
-
-    //Modal
-
-    $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('fast');
-    });
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
-    });
-    $('.button_catalog').each(function(i) {
-        $(this).on('click',function(){
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('fast');
-        });
-    });
-
-
-    function validateForms(form) {
-        $(form).validate({
-            rules: {
-                name:{
-                    required:true,
-                    minlength: 2
-                },
-                phone:"required",
-                email: {
-                    required:true,
-                    email:true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символа!")
-                },
-                phone: "Пожалуйста, введите свой номер телефон",
-                email: {
-                    required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно введен адрес почты"
-                }
-            }
-        });
-    };
-
-    validateForms('#consultation-form');
-    validateForms('#order form');
-    validateForms('#consultation form');
-
-    $('input[name=phone]').mask("+7 (999) 999-99-99");
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST", 
-            url:"../mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $('#consultation, #order').fadeOut('fast');
-            $('.overlay, #thanks').fadeIn('fast');
-            $('form').trigger('reset');
-        });
-        return false;
-    });
-
-    //Smoth scroll and page up
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 800)  {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    });
-
-    $("a[href^='#UP']").click(function() {
-        const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-        return false;
-    });
-
-    new WOW().init();
-});
+};
