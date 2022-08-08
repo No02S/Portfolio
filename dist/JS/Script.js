10227ae51cf1e97d8cbd01877f44260a8448fb83
Dima <dima.zabolotno.02@mail.ru>
2022-08-08 03:25:24 +0300

    Last layer

--- dist/JS/Script.js
+++ dist/JS/Script.js
@@ -10,6 +10,20 @@ const slider = tns({
         }   
     }
 });
+const hamburger = document.querySelector('.header__hamb'),
+        menu = document.querySelector('.menu'),
+        blackout = document.querySelector('.navigation__blackout'),
+        close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+    blackout.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+    blackout.classList.remove('active');
+});
 
 document.querySelector('.Prev').onclick = function () {
     slider.goTo('prev');
