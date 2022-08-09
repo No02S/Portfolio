66fd1a739eb14ec71205280a96dc9e1f5a56ec63
Dima <dima.zabolotno.02@mail.ru>
2022-08-09 03:37:35 +0300

    Setting mailer

--- src/JS/Script.js
+++ src/JS/Script.js
@@ -13,9 +13,10 @@ const slider = tns({
 const hamburger = document.querySelector('.header__hamb'),
         menu = document.querySelector('.menu'),
         blackout = document.querySelector('.navigation__blackout'),
-        close = document.querySelector('.menu__close');
+        close = document.querySelector('.menu__close'),
+        menufer = document.querySelectorAll('.menu__ref');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', function() {
     menu.classList.add('active');
     blackout.classList.add('active');
 });
@@ -25,6 +26,13 @@ close.addEventListener('click', () => {
     blackout.classList.remove('active');
 });
 
+menufer.forEach(function (item) {
+    item.addEventListener('click', function() {
+        menu.classList.remove('active');
+        blackout.classList.remove('active');
+    });
+});
+
 document.querySelector('.Prev').onclick = function () {
     slider.goTo('prev');
 };
@@ -32,3 +40,22 @@ document.querySelector('.Prev').onclick = function () {
 document.querySelector('.Next').onclick = function () {
     slider.goTo('next');
 };
+
+$(document).ready(function() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST", 
+            url:"../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#thanks').fadeIn('fast');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    $('.contacts__thanks-close').on('click', function() {
+        $('#thanks').fadeOut('fast');
+    });
+});
