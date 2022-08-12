c7b7a39807a7742bfabfcec4bdc769e068383741
Dima <dima.zabolotno.02@mail.ru>
2022-08-12 15:58:50 +0300

    setings

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dima.zabolotnov.02@mail.ru';                 // Наш логин
-$mail->Password = 'pkg2MHCPwMndu3Dny6Ky';                           // Наш пароль от ящика
+$mail->Username = 'dima.zabolotnov.03@mail.ru';                 // Наш логин
+$mail->Password = 'xihqok-tyBbe7-cobmuw';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('dima.zabolotnov.02@mail.ru', 'Portfolio');   // От кого письмо 
-$mail->addAddress('dima.zabolotnov.03@mail.ru');     // Add a recipient
+$mail->setFrom('dima.zabolotnov.03@mail.ru', 'Portfolio');   // От кого письмо 
+$mail->addAddress('dima.zabolotnov.02@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
