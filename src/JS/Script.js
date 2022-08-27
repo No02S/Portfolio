949298a23d8073a1f6b727ad5ed80a5c642b35b3
Dima <dima.zabolotno.02@mail.ru>
2022-08-27 05:45:38 +0300

    Settings

--- src/JS/Script.js
+++ src/JS/Script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const hamburger = document.querySelector('.header__hamb'),
         menu = document.querySelector('.menu'),
         blackout = document.querySelector('.navigation__blackout'),
@@ -8,12 +9,56 @@ const hamburger = document.querySelector('.header__hamb'),
         arrows = document.querySelectorAll('.skills__arow'),
         toolsAll = document.querySelector('.skills__tools'),
         toolsSvgs = toolsAll.querySelectorAll('svg');
+
 window.onload = function () {
     document.body.classList.add('loaded');
 };
 
-new WOW().init();
+window.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelectorAll('hr','.hrs').forEach(line =>{
+        line.style.display = "inline";
+    });
 
+    const hrs = { 
+        vertAbout:document.querySelector('.hrs__vertical_about'),
+        horAbout: document.querySelector('.hrs__horizontal_about'),
+        vertUnder: document.querySelector('.hrs__vertical_about-undertitle'),
+        vertAboutRight: document.querySelector('.hrs__vertical_about-right'),
+        };
+
+    if (document.documentElement.clientWidth <= 768) { 
+        if (document.documentElement.clientWidth <= 600) {
+            reSize("169px","calc(100% - 41px)","30px","calc(100% - 67px)");
+        } else {
+            reSize("169px","calc(100% - 75px)","30px","calc(100% - 67px)");
+        }
+    } else {
+        if (document.documentElement.clientWidth <= 1186) { 
+            reSize("220px","calc(100% - 75px)","16px","calc(100% - 119px)");
+        } else {
+            reSize("220px","calc(100% - 52px)","16px","calc(100% - 119px)");
+        }
+    }
+
+    function reSize (value1,value2,value3,value4) {
+        timeOut(hrs.vertAbout,"height",2750,value1);
+        timeOut(hrs.horAbout,"width",4750,value2);
+        timeOut(hrs.vertUnder,"height",5300,value3);
+        timeOut(hrs.vertAboutRight,"height",6750,value4);
+        function timeOut (stroke,nav,delay,value) {
+            setTimeout(function (){
+                if (nav == "height") { 
+                    stroke.style.height = value;
+                } else if (nav =="width") { 
+                    stroke.style.width = value;
+                }
+            },delay);
+        }   
+    }
+});
+
+new WOW().init();
 hamburger.addEventListener('click', function() {
     menu.classList.add('active');
     blackout.classList.add('active');
